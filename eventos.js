4094d86c7e6ab73351dc5a3d61f9b930099c9287
Vituinho <trabalhovictor531@gmail.com>
2025-12-12 08:29:48 -0300

    feat: Eventos do mouse parte 2

--- eventos.js
+++ eventos.js
@@ -36,4 +36,45 @@ $(document).ready(() => {
         console.log(e.offsetX, e.offsetY)
         $('#resultadoDiv').html(` X: ${e.offsetX} | Y: ${e.offsetY}`)
     })
+
+    /*mouseover / mouseout (antigos) */
+
+    $('#divExterna').mouseover(() => {
+        $('#resultadoDiv1').html('entrou')
+    })
+
+    $('#divExterna').mouseout(() => {
+        $('#resultadoDiv1').html('saiu')
+    })
+
+    $('#divInterna').mouseover(() => {
+        $('#resultadoDiv2').html('entrou')
+    })
+
+    $('#divInterna').mouseout(() => {
+        $('#resultadoDiv2').html('saiu')
+    })
+
+    // mouseover/mouseout é bugado, tem efeito bubble que é coco, melhor é usar o mouseenter/mouseleave
+
+    /*mouseenter / mouseleave (novos) */
+
+    $('#divExterna').mouseenter(() => {
+        $('#resultadoDiv1').html('entrou')
+    })
+    
+    $('#divExterna').mouseleave(() => {
+        $('#resultadoDiv1').html('saiu')
+    })
+
+    $('#divInterna').mouseenter(() => {
+        $('#resultadoDiv2').html('entrou')
+    })
+    
+    $('#divInterna').mouseleave(() => {
+        $('#resultadoDiv2').html('saiu')
+    })
+
+    
+
 })
