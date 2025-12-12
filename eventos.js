1f535b3fd60e2d14691b2bd77e38702c174f39a5
Vituinho <trabalhovictor531@gmail.com>
2025-12-12 08:20:05 -0300

    feat: Eventos do mouse parte 1

--- eventos.js
+++ eventos.js
@@ -19,4 +19,21 @@ $(document).ready(() => {
     $('#div1').scroll(e => {
         $(e.target).css('background-color', 'blue')
     })
+
+    $('#btn1').mousedown(() => {
+        $('#div2').css('background-color', 'green')
+    })
+
+    $('#btn1').click(() => {
+        $('#div2').css('background-color', 'blue')
+    })
+
+    $('#btn2').dblclick(() => {
+        $('#div2').css('background-color', 'yellow')
+    })
+
+    $('#div2').mousemove((e) => {
+        console.log(e.offsetX, e.offsetY)
+        $('#resultadoDiv').html(` X: ${e.offsetX} | Y: ${e.offsetY}`)
+    })
 })
